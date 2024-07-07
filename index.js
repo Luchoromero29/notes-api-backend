0b47f1f0a4ef2c2741028847880e9e4d85d76ba7
Luchoromero29 <luchonromero@gmail.com>
2024-07-07 12:08:59 -0300

    eliminacion de archivos inutilizados

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import express from 'express'
-import logger from './loggerMiddleware.js'
-import cors from 'cors'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
 
-app.use(cors())
-app.use(logger)
 
 let notes = [
     {
